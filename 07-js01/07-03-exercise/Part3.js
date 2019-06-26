e89e07c5e38ae833226f6700a4d11d070cabcec1
Long Trac <long.trac@utsa.edu>
2019-06-26 10:05:17 -0500

    js beginner

---
+++ 07-js01/07-03-exercise/Part3.js
@@ -0,0 +1,6 @@
+// this is my own version
+var lb = prompt("Please enter a number in lb for conversion")
+var kg = lb * 0.454
+alert(lb+" lb is equivalent to "+kg+" kg")
+console.log("Conversion completed")
+
